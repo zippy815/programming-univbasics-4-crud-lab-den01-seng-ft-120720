2e3454639ae571ccf2ea6913b94b73e5f016ab58
Zipporah Horch <zipporahhorch@gmail.com>
2020-11-04 21:35:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+   array = ["wow", "I", "am", "really", "learning"]
+   array
 end
 
 def retrieve_element_from_index(array, index_number)
