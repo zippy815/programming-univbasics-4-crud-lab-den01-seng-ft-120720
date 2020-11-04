b11f248a8892d404d558a142122f0474762dc26d
Zipporah Horch <zipporahhorch@gmail.com>
2020-11-04 21:40:44 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,47 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  arrays = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+   array = ["wow", "I", "am", "really", "learning"]
+   arrays = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+   array = ["wow", "I", "am", "really", "learning"]
+   array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+   array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   array[5]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array = ["wow", "I", "am", "really", "learning"]
+   array[4]= "totally"
 end
