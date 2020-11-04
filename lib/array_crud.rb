98fa0e763f28ccf7340cf82b85890993692f2eea
Zipporah Horch <zipporahhorch@gmail.com>
2020-11-04 21:31:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array.push("wow")
 end
 
 def remove_element_from_end_of_array(array)
