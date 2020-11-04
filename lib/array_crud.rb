dc038404da3501443b52445b3537d99f7eca8ca6
Zipporah Horch <zipporahhorch@gmail.com>
2020-11-04 21:40:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,11 +32,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+   array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   array[5]
 end
 
 def update_element_from_index(array, index_number, element)
